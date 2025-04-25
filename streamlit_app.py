acc005fa75eb5385e8cb5a34c52c8671c05f5b29
joshuasohga <sohjoshua92@gmail.com>
2025-04-25 16:11:30 +0800

    Update streamlit_app.py
    Rectified indentation errors.

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,10 +45,10 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
 
+    if time_to_insert:
+          session.sql(my_insert_stmt).collect()
+
+          st.success('Your Smoothie is ordered!', icon="✅")
+
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
-
-    if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-
-            st.success('Your Smoothie is ordered!', icon="✅")
